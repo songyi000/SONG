5efc04bb0a86e94a53aa7c85ec0553b8ee4eba2b
songyi000 <hyunyi02@naver.com>
2023-06-25 18:32:44 +0900

    15
    15

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// slide menu 
-$('div.top_nav_wrap ul.gnb > li').mouseenter(()=>{
-  $('div.top_nav_wrap ul.gnb > li > ul').stop().slideDown();
-});
-$('div.top_nav_wrap ul.gnb > li').mouseleave(()=>{
-  $('div.top_nav_wrap ul.gnb > li > ul').stop().slideUp();
-});
-
 // toggle menu
 $('div.header__toggle_button').click(()=> {
   $('div.header__toggle_menu').show();
