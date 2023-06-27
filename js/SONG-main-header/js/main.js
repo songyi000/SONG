028609c0b676d6672e12bc49e3db1bb6e01b4d0f
songyi000 <hyunyi02@naver.com>
2023-06-27 16:25:26 +0900

    16
    16

--- js/SONG-main-header/js/main.js
+++
@@ -1,7 +0,0 @@
-// toggle menu
-$('div.header__toggle_button').click(()=> {
-  $('div.header__toggle_menu').show();
-});
-$('div.header__toggle__close__button').click(()=> {
-  $('div.header__toggle_menu').hide();
-});
--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+// slide menu 
+$('.gnb ul.menu > li').mouseenter(()=>{
+  $('.gnb ul.menu > li > ul').stop().slideDown();
+});
+$('.gnb ul.menu > li').mouseleave(()=>{
+  $('.gnb ul.menu > li > ul').stop().slideUp();
+});
+
+
 // toggle menu
-$('div.header__toggle_button').click(()=> {
+$('div.top-menu a').click(()=> {
   $('div.header__toggle_menu').show();
 });
 $('div.header__toggle__close__button').click(()=> {
   $('div.header__toggle_menu').hide();
 });
+
